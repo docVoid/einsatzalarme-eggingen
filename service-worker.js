7a2136b3936fc97ddabb1b3fcf00fe558c8b6c30
doc.void <81415427+docVoid@users.noreply.github.com>
2025-03-20 11:09:58 +0100

    Added offline support with caching (#2)

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,25 @@
+const CACHE_NAME = 'einsatzalarme-cache-v1';
+const STATIC_ASSETS = [
+    './',
+    'index.html',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png'
+];
+
 self.addEventListener('install', (event) => {
-    console.log('Service Worker installiert');
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(STATIC_ASSETS);
+        })
+    );
+    console.log('Service Worker installiert und Assets gecached');
 });
 
 self.addEventListener('fetch', (event) => {
-    console.log('Fetch-Anfrage für:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
